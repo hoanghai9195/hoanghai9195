de9d388670b9f50ba4ad8cd89482b67900c7763a
hoanghai9195 <matranlife91.hh@gmail.com>
2021-03-15 19:14:34 +0700

    hai

---
+++ Javaa/practice/src/Bank.java
@@ -0,0 +1,15 @@
+
+
+public class Bank {
+    private double balance;
+    private double rate;
+
+    public Bank(double balance, double rate) {
+        this.balance = balance;
+        this.rate = rate;
+    }
+
+    public double caculateInterest() {
+        return balance * (rate/1200);
+    }
+}
